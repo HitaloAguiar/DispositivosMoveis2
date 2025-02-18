d95020994e4dcbdbcb57606f5a8f110e27eae2b4
HitaloAguiar <hitalo.aguiar3767@gmail.com>
2025-02-18 08:40:02 -0300

    commit inicial

---
+++ Aula01/q2.dart
@@ -0,0 +1,12 @@
+void main() {
+
+  int numeroInteiro = 10;
+  double numeroDouble = 2.5;
+  String string = "string";
+  bool valorBooleano = true;
+
+  print(numeroInteiro);
+  print(numeroDouble);
+  print(string);
+  print(valorBooleano);
+}
